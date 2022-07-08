12c6932bea969ff5bd83a7d73d68bd1f93567f4a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-08 00:29:20 -0400

    add create controller

--- app/controllers/api/v1/wordle_words_controller.rb
+++ app/controllers/api/v1/wordle_words_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::WordleWordsController < ApplicationController
     
     def index
-        wordle_words = WordleWords.all
+        wordle_word = WordleWords.all
         render json: wordle_words
     end
 
+    def create
+        wordle_word = WordleWords.new(wordle_word_params)
+        
+        if wordle_word.save
+            render json: wordle_word, status: :created
+        else
+            render json: wordle_word.errors, status: :unprocessable_entity
+        end
+    end
 end
